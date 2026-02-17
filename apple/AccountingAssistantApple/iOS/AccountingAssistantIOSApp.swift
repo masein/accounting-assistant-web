ffb76192c5f0d74a4fb152573f71d3707c2bed34
masein <imasein@gmail.com>
2026-02-17 09:26:37 +0400

    Fix iOS safe-area overlap under Dynamic Island

--- apple/AccountingAssistantApple/iOS/AccountingAssistantIOSApp.swift
+++ apple/AccountingAssistantApple/iOS/AccountingAssistantIOSApp.swift
@@ -4,12 +4,7 @@ import SwiftUI
 struct AccountingAssistantIOSApp: App {
     var body: some Scene {
         WindowGroup {
-            GeometryReader { proxy in
-                AppleChatRootView()
-                    .frame(width: proxy.size.width, height: proxy.size.height, alignment: .top)
-                    .background(Color.clear)
-            }
-            .ignoresSafeArea()
+            AppleChatRootView()
         }
     }
 }
